8d1143b45ea1091bc0917025dfcdf66d90619d81
Saso <saso@jaguar>
2019-06-19 15:48:48 +0200

    Replaced System.Drawing with ImageSharp for non-Windows platforms

--- Controllers/DecodeController.cs
+++ Controllers/DecodeController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 using System;
 using System.Drawing;
 using System.IO;
 using System.Net;
 using ZXing.Common;
 using ZXing.CoreCompat.System.Drawing;
+using ZXing.ImageSharp;
 
 namespace QRCode.Controllers
 {
@@ -20,7 +23,8 @@ namespace QRCode.Controllers
 
             using (var stream = new MemoryStream(data))
             {
-                var bitmap = Bitmap.FromStream(stream) as Bitmap;
+                //var bitmap = Bitmap.FromStream(stream) as Bitmap; //Problems with the library on non-Windows platforms
+                var bitmap = SixLabors.ImageSharp.Image.Load(stream); //Works fine
                 var result = Decode(bitmap);
 
                 //QR Code not found or cannot be decoded
@@ -48,6 +52,26 @@ namespace QRCode.Controllers
         {
             //PM> Install-Package ZXing.Net.Bindings.CoreCompat.System.Drawing
             var source = new BitmapLuminanceSource(bitmap);
+
+            //PM> Install-Package ZXing.Net
+            var reader = new BarcodeReader(null, null, ls => new GlobalHistogramBinarizer(ls))
+            {
+                AutoRotate = true,
+                TryInverted = true,
+                Options = new DecodingOptions
+                {
+                    TryHarder = true,
+                }
+            };
+
+            var result = reader.Decode(source);
+            return result;
+        }
+
+        protected ZXing.Result Decode(Image<Rgba32> bitmap)
+        {
+            //PM> Install-Package ZXing.Net.Bindings.ImageSharp
+            var source = new ImageSharpLuminanceSource<Rgba32>(bitmap);
 
             //PM> Install-Package ZXing.Net
             var reader = new BarcodeReader(null, null, ls => new GlobalHistogramBinarizer(ls))
